de64c7765cc5f7765a130ea3d88a12c3ccdcff17
Jorge7591285568 <adm.unifacs1@hotmail.com>
2024-02-09 13:40:56 +0000

    ReservaCarro

--- Cliente.java
+++ Cliente.java
@@ -1,5 +1,9 @@
 package br.edu.ifba.saj.fwads.model;
 
 public class Cliente {
+     public static void main(String[] args) {
+        List<Carro> carros = new ArrayList<>();
+      
+        }
     
 }
