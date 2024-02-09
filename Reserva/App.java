8f3a4725fd6faa4182afa3f604621d9cf12aec1f
Jorge7591285568 <adm.unifacs1@hotmail.com>
2024-02-09 13:38:24 +0000

    ReservaCarro

--- Reserva/App.java
+++ Reserva/App.java
@@ -1,10 +1,6 @@
 public class App {
     public static void main(String[] args) {
         List<Carro> carros = new ArrayList<>();
-        /*Turma t1 = new Turma("POO");
-        turmas.add(t1);
-        
-        for (int i = 0; i < 10; i++) {
-            t1.addAluno(new Aluno("Aluno "+(i+1)));*/
+      
         }
     }
