e3912041c749a4bdc453bfb506b1707527f20066
Jorge7591285568 <adm.unifacs1@hotmail.com>
2024-02-14 15:10:54 -0300

    Update Carro.java

--- Carro.java
+++ Carro.java
@@ -1,24 +1,34 @@
+
+
 public class Carro {
-    private String modelo;
+
+    private String placa;
     private String marca;
-    private int passageiro;
+    private String modelo;
     private int ano;
+    private String cor;
+    private Double diaria;
+    private Boolean disponivel;
+    private int passageiro;
 
-    public Carro(String modelo, String marca, int passageiro, int ano) {
-        this.modelo = modelo;
+    public Carro(String placa, String marca, String modelo, int ano, String cor, Double diaria, Boolean disponivel,
+            int passageiro) {
+        this.placa = placa;
         this.marca = marca;
-        this.passageiro = passageiro;
-        this.ano = ano;
-    }
-
-   
-
-    public String getModelo() {
-        return modelo;
-    }
-
-    public void setModelo(String modelo) {
         this.modelo = modelo;
+        this.ano = ano;
+        this.cor = cor;
+        this.diaria = diaria;
+        this.disponivel = disponivel;
+        this.passageiro = passageiro;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
     }
 
     public String getMarca() {
@@ -29,25 +39,59 @@ public class Carro {
         this.marca = marca;
     }
 
-    public int getPassageiro() {
-        return passageiro;
+    public String getModelo() {
+        return modelo;
     }
 
-    public void setPassageiro(int Passageiro) {
-        this.passageiro = passageiro;
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
     }
 
     public int getAno() {
         return ano;
     }
 
-    public void setAno(int Ano) {
+    public void setAno(int ano) {
         this.ano = ano;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public Double getDiaria() {
+        return diaria;
+    }
+
+    public void setDiaria(Double diaria) {
+        this.diaria = diaria;
+    }
+
+    public Boolean getDisponivel() {
+        return disponivel;
+    }
+
+    public void setDisponivel(Boolean disponivel) {
+        this.disponivel = disponivel;
+    }
+
+    public int getPassageiro() {
+        return passageiro;
+    }
+
+    public void setPassageiro(int passageiro) {
+        this.passageiro = passageiro;
     }
 
     @Override
     public String toString() {
-        return "Carro [modelo=" + modelo + ", marca=" + marca + ", passageiro=" + passageiro + ", ano=" + ano + "]";
+        return "Carro [placa=" + placa + ", marca=" + marca + ", modelo=" + modelo + ", ano=" + ano + ", cor=" + cor
+                + ", diaria=" + diaria + ", disponivel=" + disponivel + ", passageiro=" + passageiro +"]";
     }
+
 }
 
